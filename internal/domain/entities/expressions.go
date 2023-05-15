32b569ca4f46f1f7b5ab657cac773687c4a9b3cb
CapitaoDAreia <danieligord7@hotmail.com>
2023-05-15 20:58:20 -0300

    adjustments

--- internal/domain/entities/expressions.go
+++ internal/domain/entities/expressions.go
@@ -6,6 +6,5 @@ import (
 
 type Expression struct {
 	gorm.Model
-	// ID               uint64 `json:"id" gorm:"primaryKey;autoIncrement"`
 	ExpressionString string `json:"expression" gorm:"text;not null;default:null"`
 }
